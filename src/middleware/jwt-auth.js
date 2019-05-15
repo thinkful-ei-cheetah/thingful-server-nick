412d9852a284591c1f4d9d53bceeb2080d1a2c67
Nick Lee <leexnick@gmail.com>
2019-05-15 11:34:59 -0700

    refactor protected endpoints with JWT

--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -1,5 +1,41 @@
+const AuthService = require('../auth/auth-service')
+
 function requireAuth(req, res, next) {
-  next()
+  const authToken = req.get('Authorization') || ''
+
+  let bearerToken
+
+  if (!authToken.toLowerCase().startsWith('bearer ')) {
+    return res.status(401).json({ error: 'Missing bearer token' })
+  } else {
+    bearerToken = authToken.slice(7)
+  }
+
+  try {
+    const payload = AuthService.verifyJwt(bearerToken)
+    
+    AuthService.getUserwithUsername(
+      req.app.get('db'),
+      payload.sub
+    )
+      .then(user => {
+        if (!user) {
+          return res.status(401).json({ error: 'Unauthorized request' })
+        }
+
+        req.user = user
+        next()
+      })
+      .catch(err => {
+        // eslint-disable-next-line no-console
+        console.error(err)
+        next(err)
+      })
+  } catch(error) {
+    res.status(401).json({
+      error: 'Unauthorized request'
+    })
+  }
 }
 
 module.exports = {
